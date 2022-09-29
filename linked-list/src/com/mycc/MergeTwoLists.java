9676908f162d0c4019d9997e6d38533285d9b39c
mayongchang <mayongchang@piesat.cn>
2022-09-29 10:07:23 +0800

    调整util

--- linked-list/src/com/mycc/MergeTwoLists.java
+++ linked-list/src/com/mycc/MergeTwoLists.java
@@ -23,18 +23,19 @@ import java.util.List;
  *
  */
 public class MergeTwoLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
     return null;
     }
 
-    public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
 
-        ListNode node = ListNodeUtil.list2Node(list);
-        while(node!=null){
-            System.out.println(node.val);
-            node=node.next;
-        }
+    public static  void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list2 = Arrays.asList( 2, 3, 4, 5,6);
+//        System.out.println(list.size());
+        ListNode node1 = ListNodeUtil.list2Node(list1);
+        ListNode node2 = ListNodeUtil.list2Node(list2);
+        ListNode listNode = mergeTwoLists(node1, node2);
+        System.out.println("ListNodeUtil.node2List(listNode) = " + ListNodeUtil.node2List(listNode));
 
     }
 }
